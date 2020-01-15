9d3fdbcce02b3a61ce83883440796063153680b4
wildfirefans <1478006716@qq.com>
2020-01-15 05:30:41 +0000

    update

--- alien.py
+++ alien.py
@@ -2,17 +2,17 @@ import pygame
 from pygame.sprite import Sprite
 
 class Alien(Sprite):
-	
+    #define class alien
     def __init__(self,screen,setting):
-    	super().__init__()
-    	self.screen=screen
-    	self.setting=setting
-    	self.alien_image=pygame.image.load("ship2.jpg")
-    	self.alien_rect=self.alien_image.get_rect()
-    	self.alien_rect.x=self.alien_rect.width
-    	self.alien_rect.y=self.alien_rect.height
-	
+        super().__init__()
+        self.screen=screen
+        self.setting=setting
+        self.alien_image=pygame.image.load("ship2.jpg")
+        self.alien_rect=self.alien_image.get_rect()
+        self.alien_rect.x=self.alien_rect.width
+        self.alien_rect.y=self.alien_rect.height
+    #define alien method
     def alien_blit(self):
-    	self.screen.blit(self.alien_image,self.alien_rect)
+        self.screen.blit(self.alien_image,self.alien_rect)
     def alien_move(self):
-    	self.alien_rect.bottom +=self.setting.alien_speed
+        self.alien_rect.bottom +=self.setting.alien_speed
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,4 @@
 import pygame
-#import sys
 from pygame.locals import *
 from setting import Setting
 from ship import Ship
@@ -7,7 +6,6 @@ import game_function as game_f
 from bullet import Bullet
 from pygame.sprite import Group
 from alien import Alien
-
 def run_game():
     pygame.init()
     setting=Setting()
@@ -16,16 +14,12 @@ def run_game():
     ship=Ship(screen)
     alien=Alien(screen,setting)
     bullets=Group()
-    
-    while True: 
-        #alien.alien_move()
+    while True:
         game_f.check_event(ship,screen,setting,bullets)
-	    #ship.ship_move(setting)
-	    for bullet in bullets:
-	        bullet.bullet_move()
-	        if bullet.bullet_rect.top<=0:
-	        	bullets.remove(bullet)
-	    game_f.screen_update(setting,screen,ship,bullets,alien)
+        for bullet in bullets:
+            bullet.bullet_move()
+            if bullet.bullet_rect.top<=0:
+                bullets.remove(bullet)
+        game_f.screen_update(setting,screen,ship,bullets,alien)
 
 run_game()
-	
